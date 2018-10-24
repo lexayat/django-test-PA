2bc1100691e04bccd5a9480f7af0f48aed78d14a
Alexey <lexayat@mail.ru>
2018-10-24 17:16:29 +0300

    solution of the CORS problem

--- APItusa/settings.py
+++ APItusa/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from corsheaders.defaults import default_methods
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -144,4 +145,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #CORS settings
 CORS_ORIGIN_ALLOW_ALL = True #Allow all requests
+
+CORS_ALLOW_METHODS = default_methods
 
