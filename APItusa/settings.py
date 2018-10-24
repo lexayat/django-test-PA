aa55d782aab1fe607271731ce88a0b39faee56ee
Alexey <lexayat@mail.ru>
2018-10-24 16:54:10 +0300

    Changed SECRET KEY

--- APItusa/settings.py
+++ APItusa/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qmm(c+0!#kl3xbc2c5$^=8s!hn(c$itm!#wdill#4=b(&-ir=m'
+SECRET_KEY = os.environ.get('SECTRET_KEY_DJANGO')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
