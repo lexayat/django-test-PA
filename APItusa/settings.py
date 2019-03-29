4672afea4b6139378158e3de93a4b9ed78405c09
Alexey <lexayat@mail.ru>
2019-03-29 03:36:36 +0300

    changed allowed hosts

--- APItusa/settings.py
+++ APItusa/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECTRET_KEY_DJANGO')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -155,4 +155,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CORS_ORIGIN_ALLOW_ALL = True #Allow all requests
 
 CORS_ALLOW_CREDENTIALS = True
+
+ACCOUNT_LOGOUT_ON_GET = True
 
--- basic/models.py
+++ basic/models.py
@@ -2,7 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+
+
+
 class Location(models.Model):
     lat = models.FloatField()
     lng = models.FloatField()
     secure_radius = models.IntegerField(default=1000)
+
+
+#class Tusa(models.Model):
+    #category = models.CharField()
+   # alcohol = models.BooleanField(default=True)
+   # min_age = models.IntegerField()
+   # max_age = models.IntegerField()
+   # date_time = models.DateTimeField()
+    #creator_comment = models.CharField()
+    #members = models.ManyToManyField()
